11cce5b7c5b97b1c29f7b1e1d88975fac822b37f
rishitha.menusha <rishitha.menush@gmail.com>
2024-10-10 15:58:38 +0530

    Update the splash and auth

---
+++ lib/page/main/jobs/jobs_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class JobsPage extends StatefulWidget {
+  const JobsPage({super.key});
+
+  @override
+  State<JobsPage> createState() => _JobsPageState();
+}
+
+class _JobsPageState extends State<JobsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- lib/page/main/main_page/main_page.dart
+++ lib/page/main/main_page/main_page.dart
@@ -1,5 +1,15 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:linkedin/page/main/create/create_page.dart';
+import 'package:linkedin/page/main/home/home_page.dart';
+import 'package:linkedin/page/main/jobs/jobs_page.dart';
+import 'package:linkedin/page/main/network/network_page.dart';
+import 'package:linkedin/page/main/notifications/notifications_page.dart';
+import 'package:linkedin/theme/styles.dart';
 
+import '../widgets/app_bar_widget.dart';
+@override
 class MainPage extends StatefulWidget {
   const MainPage({super.key});
 
@@ -8,8 +18,88 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
+  final GlobalKey<ScaffoldState> _scaffoldState = GlobalKey<ScaffoldState>();
+
+  int _currentPageIndex = 0;
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return  Scaffold(
+      drawer: const Drawer(),
+      key: _scaffoldState,
+      appBar: appBarWidget(
+        context,
+        title: "Search",
+        isJobsTab: false,
+        onLeadingTapClickListener: (){
+          setState((){
+            _scaffoldState.currentState!.openDrawer();
+          });
+        }
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentPageIndex,
+        onTap: (index){
+          setState(() {
+            _currentPageIndex = index;
+          });
+        },
+        selectedItemColor: linkedInBlack000000,
+        selectedLabelStyle: const TextStyle(color: linkedInBlack000000),
+        unselectedItemColor: linkedInMediumGrey86888A,
+        unselectedLabelStyle: const TextStyle(color: linkedInMediumGrey86888A),
+        showSelectedLabels: true,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.house_fill),
+            label: "Home"
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(FontAwesomeIcons.userGroup),
+            label: "Network"
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.add_box, size: 30,),
+            label: "Post"
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.notifications,
+              size: 30,
+            ),
+            label: "Notifications",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(FontAwesomeIcons.briefcase),
+            label: "Jobs"
+          )
+        ],
+      ),
+      body: _switchPages(_currentPageIndex)
+    );
+  }
+
+  _switchPages(int index) {
+    switch(index){
+      case 0:
+        {
+          return HomePage();
+        }
+      case 1:
+        {
+          return NetworkPage();
+        }
+      case 2:
+        {
+          return CreatePage();
+        }
+      case 3:
+        {
+          return NotificationsPage();
+        }
+      case 4:
+        {
+          return JobsPage();
+        }
+    }
   }
 }
