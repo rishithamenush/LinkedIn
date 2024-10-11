a5e2ab3057d15c2816db47397154cdf0307b8f9d
rishitha.menusha <rishitha.menush@gmail.com>
2024-10-11 18:32:39 +0530

    Update the network page

--- lib/page/main/network/network_page.dart
+++ lib/page/main/network/network_page.dart
@@ -2,6 +2,8 @@ import 'dart:ui';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:linkedin/data/network_entity.dart';
+import 'package:linkedin/page/main/network/widgets/single_network_user_widget.dart';
 import 'package:linkedin/theme/styles.dart';
 
 class NetworkPage extends StatefulWidget {
@@ -12,78 +14,95 @@ class NetworkPage extends StatefulWidget {
 }
 
 class _NetworkPageState extends State<NetworkPage> {
+  List<NetworkEntity> networkData = NetworkEntity.networkData;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          const SizedBox(
-            height: 10,
-          ),
-          const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 10),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  "Manage My Network",
-                  style: TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
-                    color: linkedInBlue0077B5
-                  ),
-                ),
-                Icon(
-                  Icons.arrow_forward_ios,
-                  color: linkedInLightGreyCACCCE,
-                ),
-              ],
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 10,
             ),
-          ),
-          const SizedBox(
-            height: 15,
-          ),
-          Container(
-            width: double.infinity,
-            height: 8,
-            color: linkedInLightGreyCACCCE,
-          ),
-          const SizedBox(
-            height: 15,
-          ),
-          const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 10.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  "Invitations",
-                  style: TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
-                    color: linkedInBlue0077B5
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Manage My Network",
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: linkedInBlue0077B5
+                    ),
                   ),
-                ),
-                Icon(
-                  Icons.arrow_forward_ios,
-                  color: linkedInMediumGrey86888A,
-                )
-              ],
+                  Icon(
+                    Icons.arrow_forward_ios,
+                    color: linkedInLightGreyCACCCE,
+                  ),
+                ],
+              ),
             ),
-          ),
-          const SizedBox(
-            height: 15,
-          ),
-          Container(
-            width: double.infinity,
-            height: 8,
-            color: linkedInLightGreyCACCCE,
-          ),
-          const SizedBox(
-            height: 15,
-          ),
-          
-        ],
+            const SizedBox(
+              height: 15,
+            ),
+            Container(
+              width: double.infinity,
+              height: 8,
+              color: linkedInLightGreyCACCCE,
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 10.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    "Invitations",
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: linkedInBlue0077B5
+                    ),
+                  ),
+                  Icon(
+                    Icons.arrow_forward_ios,
+                    color: linkedInMediumGrey86888A,
+                  )
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            Container(
+              width: double.infinity,
+              height: 8,
+              color: linkedInLightGreyCACCCE,
+            ),
+            const SizedBox(
+              height: 15,
+            ),
+            GridView.builder(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              shrinkWrap: true,
+              physics: const ScrollPhysics(),
+              itemCount: networkData.length,
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  mainAxisSpacing: 6,
+                  crossAxisSpacing: 6,
+                  childAspectRatio: 0.6),
+              itemBuilder: (context, index) {
+                final network = networkData[index];
+                return SingleNetworkUserWidget(network: network);
+              },
+            )
+          ],
+        ),
       ),
     );
   }
